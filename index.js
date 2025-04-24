067e49bc63fae51c4f88fc22eb808364f276ff57
Rosemary Hogarth <rohogarth@gmail.com>
2025-04-24 12:10:46 +0200

    Altered homepage and menu

--- index.js
+++ index.js
@@ -6,23 +6,14 @@ image.addEventListener("click", function() {
   window.location.pathname = "/about";
 })
 }
+
+
   const navbar = document.querySelector(".navbar");
 
   if (navbar && window.location.pathname === "/") {
-    navbar.style.backgroundColor = "white";
-    const menuIcon = document.querySelector(".menu-icon");
-    menuIcon.style.display = "none"
+    navbar.style.display = "none";
+
   }
 
 
-  const logoLink = document.querySelector(".logo a");
-  if (logoLink) {
-    if (window.location.pathname === "/") {
-
-      logoLink.setAttribute("href", "/about");
-    } else {
-
-      logoLink.setAttribute("href", "/");
-    }
-  }
 });
