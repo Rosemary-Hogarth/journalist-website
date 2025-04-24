bbdc7e9b63b35d43aa508500c79a56322a210473
Rosemary Hogarth <rohogarth@gmail.com>
2025-04-24 09:32:51 +0200

    Added mav to the homepage

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 document.addEventListener("DOMContentLoaded", function() {
 const image = document.querySelector(".full-screen-image")
 
+if (image){
 image.addEventListener("click", function() {
   window.location.pathname = "/about";
 })
-
+}
   const navbar = document.querySelector(".navbar");
 
   if (navbar && window.location.pathname === "/") {
-    navbar.style.display = "none";
+    navbar.style.backgroundColor = "white";
+    const menuIcon = document.querySelector(".menu-icon");
+    menuIcon.style.display = "none"
   }
 
 
+  const logoLink = document.querySelector(".logo a");
+  if (logoLink) {
+    if (window.location.pathname === "/") {
 
-  setTimeout(function () {
-    const homeName = document.querySelector(".home-name");
-    homeName.classList.add("show"); // Add 'show' class after 2 seconds to trigger the fade-in
-  }, 1000); // Delay of 2000ms (2 seconds)
+      logoLink.setAttribute("href", "/about");
+    } else {
 
+      logoLink.setAttribute("href", "/");
+    }
+  }
 });
